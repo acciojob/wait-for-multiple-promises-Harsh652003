c723d126e69b14e72ee2ab03d62e8545ac649f3c
Harsh652003 <156173288+Harsh652003@users.noreply.github.com>
2024-09-29 01:30:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-//your JS code here. If required.
-
+// Function to generate a random time between 1 and 3 seconds
 function getRandomTime() {
-    return Math.random() * 2000 + 1000; 
+    return Math.random() * 2000 + 1000;  // Time in ms between 1000 and 3000
 }
 
-
+// Function to create a promise that resolves after a random time
 function createPromise(name) {
     const time = getRandomTime();
     return new Promise((resolve) => {
         setTimeout(() => resolve({ name, time }), time);
     });
 }
+
+// Array of three promises
 const promises = [
     createPromise('Promise 1'),
     createPromise('Promise 2'),
     createPromise('Promise 3')
 ];
 
-
+// Get the table body and show a loading message
 const tableBody = document.getElementById('output');
 tableBody.innerHTML = '<tr><td colspan="2">Loading...</td></tr>';
 
-
+// Track the start time
 const startTime = Date.now();
 
-
+// Wait for all promises to resolve
 Promise.all(promises).then((results) => {
-    const totalTime = (Date.now() - startTime) / 1000;
+    const totalTime = (Date.now() - startTime) / 1000;  // Total time in seconds
 
-  
+    // Clear the loading message
     tableBody.innerHTML = '';
 
-    
+    // Populate the table with the results of each promise
     results.forEach((result) => {
         const row = `
             <tr>
@@ -42,7 +43,7 @@ Promise.all(promises).then((results) => {
         tableBody.innerHTML += row;
     });
 
-    
+    // Add a final row for the total time
     const totalRow = `
         <tr>
             <td>Total</td>
